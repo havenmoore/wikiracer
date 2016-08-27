653741139d998a2e09a4cbca879729090f9a0db9
Haven Moore <haven.moore@gmail.com>
2016-08-26 17:07:48 -0700

    Add separate queues for paths of different lengths. Check each link against pages linked to by end path.

--- wikiracer.py
+++ wikiracer.py
@@ -26,17 +26,34 @@ def find_path(start, end):
     if start == end:
         return [start, end]
     visited_pages = set()
-    path_queue = [[start]]
-    while path_queue:
-        current_path = path_queue.pop(0)
+
+    end_links = get_page_links(end)
+
+    path_queues = {1: [[start]] }
+    current_path_queue_key = 1
+    current_path_queue = path_queues[1]
+
+    while current_path_queue:
+        current_path = current_path_queue.pop(0)
         current_page = current_path[-1]
         if current_page not in visited_pages:
             visited_pages.add(current_page)
-            print('{}/{}'.format(len(visited_pages),len(path_queue)+len(visited_pages)))
+            print(len(visited_pages))
             page_links = get_page_links(current_page)
             if end in page_links:
                 return current_path + [end]
-            path_queue.extend([current_path + [page_link] for page_link in page_links])
+            for page_link in page_links:
+                if page_link in end_links:
+                    path_queues.setdefault(len(current_path)+1, []).insert(0, current_path+[page_link])
+                else:
+                    path_queues.setdefault(len(current_path)+1, []).append(current_path+[page_link])
+
+        if not current_path_queue:
+            current_path_queue_key += 1
+            if current_path_queue_key in path_queues:
+                current_path_queue = path_queues[current_path_queue_key]
+            else:
+                return None
     return None
 
 if __name__ == "__main__":
